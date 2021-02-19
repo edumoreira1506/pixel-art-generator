fd62a948137e5aab29ab5851961b1a5976a60050
Eduardo Moreira <contato@eduardoem.com.br>
2021-02-19 14:04:05 -0300

    Add simple board component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import React, { ReactElement } from 'react';
 
 import Header from './components/Header';
+import Board from './components/Board/Board';
 
 const App = (): ReactElement => (
-  <Header />
+  <>
+    <Header />
+    <Board />
+  </>
 );
 
 export default App;
---
+++ src/components/Board/Board/index.tsx
@@ -0,0 +1,9 @@
+import React, { ReactElement } from 'react';
+
+import { StyledBoard } from './styles';
+
+const Board = (): ReactElement => (
+  <StyledBoard>Board!</StyledBoard>
+);
+
+export default Board;
