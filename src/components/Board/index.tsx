abdcc322e06698644b04d798ea017c327e5824da
Eduardo Moreira <contato@eduardoem.com.br>
2021-04-20 17:48:51 -0300

    Add option to user draw

--- src/components/Board/index.tsx
+++ src/components/Board/index.tsx
@@ -1,29 +1,62 @@
-import React, { ReactElement } from 'react';
+import React, { Dispatch, ReactElement, SetStateAction, useEffect, useRef, useState } from 'react';
 
 import { StyledBoard, StyledBoardItem, StyledBoardRow } from './styles';
 
-import { BoardItemType } from './types';
-
 type BoardPropsType = {
-  items: Array<Array<BoardItemType>>;
+  items: Array<Array<string>>;
   margin: number;
+  onChange: Dispatch<SetStateAction<any[]>>;
+  width: number;
 };
 
-const Board = ({ items, margin }: BoardPropsType): ReactElement => (
-  <StyledBoard>
-    {items.map((columnItems, columnIndex) => 
-      <StyledBoardRow key={columnIndex}>
-        {columnItems.map((item, itemIndex) => (
-          <StyledBoardItem
-            margin={margin}
-            color={item.color}
-            key={`board-item-${itemIndex}-${item.color}`}
-            size={item.size}
-          />
-        ))}
-      </StyledBoardRow>
-    )}
-  </StyledBoard>
-);
+export default function Board({ items, margin, onChange, width }: BoardPropsType): ReactElement {
+  const [isDrawing, setIsDrawing] = useState(false);
+  const boardRef = useRef<any>();
 
-export default Board;
+  const setUserIsDrawing = () => setIsDrawing(true);
+  const setUserIsNotDrawing = () => setIsDrawing(false);
+
+  const handleDraw = (e: any) => {
+    if (!isDrawing || !boardRef || !boardRef.current) return;
+
+    const { clientX, clientY } = e;
+    const { left, right, bottom, top } = boardRef.current.getBoundingClientRect();
+    const isOutOfBoard = (
+      (clientX > right || clientX < left) ||
+      (clientY < top || clientY > bottom)
+    );
+
+    if (isOutOfBoard) return;
+
+    const marginSize = items.length * margin;
+    const row = Math.round(Number(clientY - top + marginSize) / width) - 1;
+    const column = Math.round(Number(clientX - left + marginSize) / width) - 1;
+
+    onChange(prevItems => prevItems.map((rowItem: Array<string>, rowIndex: number) => rowIndex === row ? (
+      rowItem.map((columnItem: string, columnIndex: number) => columnIndex === column ? '#ffffff': columnItem)
+    ) : ([ ...rowItem ])));
+  };
+
+  useEffect(() => {
+    document.body.addEventListener('mouseup', setUserIsNotDrawing);
+
+    return () => document.body.removeEventListener('mouseup', setUserIsNotDrawing);
+  });
+
+  return (
+    <StyledBoard ref={boardRef} onMouseDown={setUserIsDrawing} onMouseOver={handleDraw}>
+      {items.map((columnItems, columnIndex) => 
+        <StyledBoardRow key={columnIndex}>
+          {columnItems.map((item, itemIndex) => (
+            <StyledBoardItem
+              margin={margin}
+              color={item}
+              key={`board-item-${itemIndex}-${item}`}
+              size={width}
+            />
+          ))}
+        </StyledBoardRow>
+      )}
+    </StyledBoard>
+  );
+}
--- src/containers/Board/index.tsx
+++ src/containers/Board/index.tsx
@@ -1,23 +1,27 @@
-import React, { ReactElement } from 'react';
+import React, { ReactElement, useEffect, useState } from 'react';
 import Board from '../../components/Board';
 import { configKeys } from '../../config/constants';
 import useConfig from '../../hooks/useConfig';
+
+const setupColors = (rows: number, columns: number, color: string) => Array(rows).fill(Array(columns).fill(color));
 
 const BoardContainer = (): ReactElement => {
   const [columns] = useConfig(configKeys.COLUMNS);
   const [itemWidth] = useConfig(configKeys.ITEM_WIDTH);
   const [marginBetween] = useConfig(configKeys.MARGIN_BETWEEN);
   const [rows] = useConfig(configKeys.ROWS);
+  const [items, setItems] = useState(setupColors(rows, columns, 'red'));
+
+  useEffect(() => {
+    setItems(setupColors(rows, columns, 'red'));
+  }, [rows, columns]);
 
   return (
     <Board
-      items={
-        Array(rows).fill(Array(columns).fill({
-          color: 'red',
-          size: itemWidth
-        }))
-      }
+      items={items}
+      onChange={setItems}
       margin={marginBetween}
+      width={itemWidth}
     />
   );
 };
