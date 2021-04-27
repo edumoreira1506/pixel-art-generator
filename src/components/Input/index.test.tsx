2eabb0d002900e1a7d8a21d427716ea9220ba216
Eduardo Moreira <contato@eduardoem.com.br>
2021-04-27 20:51:28 -0300

    Add tests to input

---
+++ src/components/Input/index.test.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { fireEvent, getByTestId, render } from '@testing-library/react';
+
+import Input from '.';
+
+describe('<Input />', () => {
+  const renderInput = (overrideProps = {}) => {
+    const defaultProps = {
+      value: '',
+      onChange: jest.fn(),
+      placeholder: '',
+      type: 'text',
+      required: true
+    };
+    const { container, getByText, getByTestId } = render(<Input {...defaultProps} {...overrideProps} />);
+
+    return { container, getByText, getByTestId };
+  };
+
+  it('renders the component', () => {
+    const { container } = renderInput();
+
+    expect(container).toBeInTheDocument();
+  });
+});
